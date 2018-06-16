9c1c9042616cbf515eacf38819fcf2d647b4ed97
Titi Wangsa Damhore <ttwd80@gmail.com>
2018-06-17 08:33:02 +1000

    fake index displayed

---
+++ src/main/java/com/github/ttwd80/november/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.github.ttwd80.november;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	public void configure(final WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/");
+	}
+
+}
