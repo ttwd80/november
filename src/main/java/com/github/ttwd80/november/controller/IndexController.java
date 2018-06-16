520cf1c76ddb5e04a4c97d0ae87b4f5a7c157a0f
Titi Wangsa Damhore <ttwd80@gmail.com>
2018-06-17 09:54:15 +1000

    redirect to setup page if there are no tables yet

--- src/main/java/com/github/ttwd80/november/controller/IndexController.java
+++ src/main/java/com/github/ttwd80/november/controller/IndexController.java
@@ -1,14 +1,29 @@
 package com.github.ttwd80.november.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.github.ttwd80.november.model.service.DatabaseService;
+
 @Controller
 public class IndexController {
 
+	private final DatabaseService databaseService;
+
+	@Autowired
+	public IndexController(DatabaseService databaseService) {
+		super();
+		this.databaseService = databaseService;
+	}
+
 	@RequestMapping("/")
 	public ModelAndView index() {
-		return new ModelAndView("index");
+		if (databaseService.isSetuUp()) {
+			return new ModelAndView("index");
+		} else {
+			return new ModelAndView("redirect:/setup");
+		}
 	}
 }
