e98faeb5c4d5f758df7e5c9eb2d67abfb89fa078
Titi Wangsa Damhore <ttwd80@gmail.com>
2018-06-19 16:16:06 +1000

    find the roles based on the username

--- src/main/java/com/github/ttwd80/november/model/repository/UserRoleRepository.java
+++ src/main/java/com/github/ttwd80/november/model/repository/UserRoleRepository.java
@@ -1,9 +1,12 @@
 package com.github.ttwd80.november.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.github.ttwd80.november.model.entity.UserRole;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 
+	List<UserRole> findByUsernameUsername(String username);
 }
