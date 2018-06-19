0905b38057e641fb9584b48b220de2bf4c5924fd
Titi Wangsa Damhore <ttwd80@gmail.com>
2018-06-19 15:55:57 +1000

    added a password encoder

--- src/main/java/com/github/ttwd80/november/NovemberApplication.java
+++ src/main/java/com/github/ttwd80/november/NovemberApplication.java
@@ -2,11 +2,19 @@ package com.github.ttwd80.november;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class NovemberApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NovemberApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(10);
 	}
 }
