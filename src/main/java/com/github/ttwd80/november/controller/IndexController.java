f1bee1acb1fd6fff6494faca4eefb27e1d0711b8
Titi Wangsa Damhore <ttwd80@gmail.com>
2018-06-20 07:36:10 +1000

    main front page controller

--- src/main/java/com/github/ttwd80/november/controller/IndexController.java
+++ src/main/java/com/github/ttwd80/november/controller/IndexController.java
@@ -1,7 +1,30 @@
 package com.github.ttwd80.november.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.github.ttwd80.november.model.service.DatabaseService;
 
 @Controller
 public class IndexController {
+
+	private final DatabaseService databaseService;
+
+	@Autowired
+	public IndexController(DatabaseService databaseService) {
+		super();
+		this.databaseService = databaseService;
+	}
+
+	@RequestMapping("/")
+	public ModelAndView index() {
+		if (databaseService.isAnyUserRegistered()) {
+			return new ModelAndView("redirect:/content");
+		} else {
+			return new ModelAndView("redirect:/setup/setup");
+		}
+	}
+
 }
