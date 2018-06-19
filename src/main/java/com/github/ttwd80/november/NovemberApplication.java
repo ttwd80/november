a312d9365f396f31c715d191917bae304712a91b
Titi Wangsa Damhore <ttwd80@gmail.com>
2018-06-20 07:33:58 +1000

    security configuration

--- src/main/java/com/github/ttwd80/november/NovemberApplication.java
+++ src/main/java/com/github/ttwd80/november/NovemberApplication.java
@@ -3,10 +3,12 @@ package com.github.ttwd80.november;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@ImportResource("classpath:application-context.xml")
 public class NovemberApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/github/ttwd80/november/SecurityConfiguration.java
+++ src/main/java/com/github/ttwd80/november/SecurityConfiguration.java
@@ -1,15 +1,17 @@
 package com.github.ttwd80.november;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
-	public void configure(final WebSecurity web) throws Exception {
-		final String[] patterns = { "/", "/setup/**", "/webjars/**" };
-		web.ignoring().antMatchers(patterns);
+	public void configure(final HttpSecurity http) throws Exception {
+		final String[] patterns = { "/", "/setup/**", "/webjars/**", "/login" };
+		http.authorizeRequests().antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+				.antMatchers("/user/**", "/content").access("hasRole('ROLE_USER')").antMatchers(patterns).anonymous()
+				.and().csrf().and().formLogin();
 	}
 
 }
