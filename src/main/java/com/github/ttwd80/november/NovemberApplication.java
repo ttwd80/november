a32dc29e2bc3e592938c20f16e6f9e450bcd2bb2
Titi Wangsa Damhore <ttwd80@gmail.com>
2018-06-21 05:05:13 +1000

    build as war

--- src/main/java/com/github/ttwd80/november/NovemberApplication.java
+++ src/main/java/com/github/ttwd80/november/NovemberApplication.java
@@ -2,6 +2,8 @@ package com.github.ttwd80.november;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,7 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @ImportResource("classpath:application-context.xml")
-public class NovemberApplication {
+public class NovemberApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NovemberApplication.class, args);
@@ -18,5 +20,10 @@ public class NovemberApplication {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder(10);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NovemberApplication.class);
 	}
 }
