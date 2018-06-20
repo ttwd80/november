415ffa5db57f9e0338bb57f9b2b563ad745276eb
Titi Wangsa Damhore <ttwd80@gmail.com>
2018-06-21 07:04:35 +1000

    split web integration test. make sure before each works.

--- src/test/java/com/github/ttwd80/november/web/UserRegistrationIT.java
+++ src/test/java/com/github/ttwd80/november/web/UserRegistrationIT.java
@@ -1,5 +1,7 @@
 package com.github.ttwd80.november.web;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
@@ -10,7 +12,30 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 class UserRegistrationIT extends AbstractSeleniumIT {
 
 	@Test
-	void test() {
+	void testGivenDatabaseIsEmptyWhenWebRootIsAccessedThenUrlIsSetupSetup() {
+		webDriver.get(BASE_URL + "/");
+		webDriverWait.until(ExpectedConditions.urlContains("setup/setup"));
+		assertEquals(BASE_URL + "/setup/setup", webDriver.getCurrentUrl());
+	}
+
+	@Test
+	void testGivenUserHasAccessedSetupSetupWhenPageIsViewedThenChangePasswordFieldIsPresent() {
+		webDriver.get(BASE_URL + "/");
+		webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.name("username")));
+	}
+
+	@Test
+	void testGivenUserCanSeeThePasswordResetFormWhenUserSubmitsItThenItShouldWork() {
+		webDriver.get(BASE_URL + "/");
+		webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.name("username")));
+		webDriver.findElement(By.id("admin-password")).sendKeys("xxx");
+		webDriver.findElement(By.id("admin-password-confirm")).sendKeys("xxx");
+		webDriver.findElement(By.id("button-submit")).click();
+		webDriverWait.until(ExpectedConditions.urlContains("/login"));
+	}
+
+	@Test
+	void testGivenPasswordCanBeChangedWhenUserTriesToLoginThenItShouldWork() {
 		webDriver.get(BASE_URL + "/");
 		webDriver.findElement(By.id("admin-password")).sendKeys("xxx");
 		webDriver.findElement(By.id("admin-password-confirm")).sendKeys("xxx");
