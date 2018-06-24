ecfc43216f6d96c453a24da291a3f399a7205e04
Titi Wangsa Damhore <ttwd80@gmail.com>
2018-06-24 10:47:41 +1000

    renamed integration test to *IT

--- src/test/java/com/github/ttwd80/november/NovemberApplicationIT.java
+++ src/test/java/com/github/ttwd80/november/NovemberApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NovemberApplicationTests {
+public class NovemberApplicationIT {
 
 	@Test
 	public void contextLoads() {
