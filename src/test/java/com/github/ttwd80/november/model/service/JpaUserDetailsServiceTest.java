e031b4c75acf39e7ba84aba7672f849ffc4966b5
Titi Wangsa Damhore <ttwd80@gmail.com>
2018-06-24 12:03:31 +1000

    62% coverage

--- src/test/java/com/github/ttwd80/november/model/service/JpaUserDetailsServiceTest.java
+++ src/test/java/com/github/ttwd80/november/model/service/JpaUserDetailsServiceTest.java
@@ -5,15 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -41,18 +44,26 @@ class JpaUserDetailsServiceTest {
 		assertThrows(UsernameNotFoundException.class, () -> sut.loadUserByUsername("random"));
 	}
 
-	@Test
-	void testLoadUserByUsernameSingleRole() {
-		User user = User.builder().password("w7fk2ssk").build();
-		Role role = Role.builder().rolename("ROLE_ADMIN").build();
-		UserRole userRole = UserRole.builder().username(user).rolename(role).build();
-		List<UserRole> list = Arrays.asList(userRole);
-		when(userRoleRepository.findByUsernameUsername("user32")).thenReturn(list);
-		UserDetails userDetails = sut.loadUserByUsername("user32");
-		assertEquals("user32", userDetails.getUsername());
-		assertEquals("w7fk2ssk", userDetails.getPassword());
-		assertEquals(Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")),
-				new ArrayList<>(userDetails.getAuthorities()));
+	@ParameterizedTest
+	@CsvSource({ "user32, w7fk2ssk, 'ROLE_ADMIN'", "user33, bi68aycu, 'ROLE_ADMIN, ROLE_USER'" })
+	void testLoadUserByUsernameSingleRole(String username, String userPassword, String concatenatedUserRoles) {
+		List<UserRole> list = new ArrayList<>();
+		String[] userRoles = StringUtils.split(concatenatedUserRoles, ',');
+		User user = User.builder().username(username).password(userPassword).build();
+		for (String userRoleValue : userRoles) {
+			Role role = Role.builder().rolename(userRoleValue).build();
+			UserRole userRole = UserRole.builder().username(user).rolename(role).build();
+			list.add(userRole);
+		}
+
+		when(userRoleRepository.findByUsernameUsername(username)).thenReturn(list);
+		UserDetails userDetails = sut.loadUserByUsername(username);
+		assertEquals(username, userDetails.getUsername());
+		assertEquals(userPassword, userDetails.getPassword());
+		List<String> expectedRoles = Stream.of(userRoles).sorted().collect(Collectors.toList());
+		List<String> actualRoles = userDetails.getAuthorities().stream().map(Object::toString).sorted()
+				.collect(Collectors.toList());
+		assertEquals(expectedRoles, actualRoles);
 	}
 
 }
